05ba36ad903cbebf6d02b175559c6084971b4f30
mcordes26 <35666284+mcordes26@users.noreply.github.com>
2018-02-11 11:38:22 -0600

    created pres.py

---
+++ pres.py
@@ -0,0 +1,20 @@
+from flask import Flask # pythin web server for browser controls
+from subprocess import Popen # runs command line programs
+app = Flask(__name__) # creating actual web server
+
+
+def pix(folder): # creating function to run eeach command line name
+    
+    Popen(["/bin/bash", "/home/pi/wwt-villa/slideshow.sh", "/home/pi/wwt-villa/" + folder]) # running the images
+    return folder # returns text to display back in browser
+
+
+@app.route("/<folder>") # defining browser route
+def showpix(): # function "allClear" will run
+    return pix(folder) # running alert function and returns alert message for "allClear"
+
+
+
+
+
+app.run(host="0.0.0.0") # running the web server
