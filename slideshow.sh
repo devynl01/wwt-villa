b7f31a14ff720a8114e3a3f351e4387a6f40c530
mcordes26 <35666284+mcordes26@users.noreply.github.com>
2018-02-18 12:12:21 -0600

    sort

--- slideshow.sh
+++ slideshow.sh
@@ -2,4 +2,4 @@
 PHOTODIR=$1 # here this is the passed photo directory for images we want to run
 INTERVAL=5 #number of second to change the images
 pkill -9 fbi #stops all fbi programs that are currently running
-fbi -T 1 -d /dev/fb0 -noverbose -a -t $INTERVAL -u `find $PHOTODIR -iname "*.jpg" -o -iname "*.png"` # finding all the pictures and then putting them into fbi for slideshow
+fbi -T 1 -d /dev/fb0 -noverbose -a -t $INTERVAL -u `find $PHOTODIR -iname "*.jpg" -o -iname "*.png" | sort` # finding all the pictures and then putting them into fbi for slideshow
